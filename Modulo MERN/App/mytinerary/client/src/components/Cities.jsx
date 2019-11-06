c8a23f65473b7bc195f18fb5f08b55a6c5316c71
Leo Sanchez <sanchez.leo88@gmail.com>
2019-11-06 15:08:58 -0300

    Arreglados los archivos ignorados, implementación de lista de ciudades, instalaccion de redux

--- Modulo MERN/App/mytinerary/client/src/components/Cities.jsx
+++ Modulo MERN/App/mytinerary/client/src/components/Cities.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem } from 'reactstrap';
 
-const Cities = () => {
-  return (
-    <section className='tab cities'>
-      <h1>Cities</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-        quibusdam fugit numquam eveniet necessitatibus dolores! Harum voluptate,
-        soluta eligendi ad quis nisi aliquam temporibus, perspiciatis voluptates
-        animi aliquid cumque rem?
-      </p>
-    </section>
-  );
-};
+
+class Cities extends Component {
+  constructor() {
+    super();
+    this.state = { cities: [] }
+  }
+
+  fetchCities = () => {
+    fetch('/cities').then(response => response.json()).then(cities => this.setState({ cities })).catch(err => console.log(err))
+  }
+
+  componentDidMount() {
+    this.fetchCities()
+  }
+
+  render() {
+    return (
+      <section className='tab cities'>
+        <Container>
+          <h1>Cities</h1>
+
+          <ListGroup>
+            {this.state.cities.map(city => {
+              return (<ListGroupItem key={city._id}>
+                {city.city}, {city.country}
+              </ListGroupItem>)
+            })}
+          </ListGroup>
+
+        </Container>
+      </section>
+    );
+  };
+}
 
 export default Cities;
